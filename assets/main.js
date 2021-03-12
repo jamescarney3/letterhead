267cbcc057845427064d6460bb9ff48143f6a796
jamescarney3 <jmcarneyiii@gmail.com>
2021-03-12 18:03:20 -0500

    deal with if moment CDN doesn't work

--- assets/main.js
+++ assets/main.js
@@ -2,12 +2,41 @@ var DATE_ID = 'date-content';
 var STATEMENT_ID = 'statement-content';
 var INPUT_ID = 'text-input';
 var DATE_FORMAT = 'MMMM DD, YYYY'
+var MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
 var INPUT_HEIGHT = '36px'
+
 
 function main() {
   // just do this automatically
   var dateSpan = document.getElementById(DATE_ID);
-  dateSpan.innerHTML = moment().format(DATE_FORMAT);
+  // don't break if moment doesn't load from the CDN
+  try {
+    // format the easy way
+    dateSpan.innerHTML = moment().format(DATE_FORMAT);
+  } catch {
+    // complain
+    console.log('moment CDN is either blocked or down, shameful');
+    // if not, do it the old fashioned way
+    var date = new Date();
+    var month = MONTHS[date.getMonth()];
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var dateString = ''.concat(month, ' ', day, ', ', year); // string concatenation folks, we love it don't we
+    dateSpan.innerHTML = dateString;
+  }
 
   // input => statement event listener
   var input = document.getElementById(INPUT_ID);
@@ -19,15 +48,7 @@ function main() {
     statementSpan.innerHTML = e.target.value;
   });
 
-
+  console.log('onload function run successfully!');
 }
-
-function handleInputChange(e) {
-  console.log('firing!');
-  console.log(e);
-}
-
-console.log('all good!');
-
 
 window.onload = main;
